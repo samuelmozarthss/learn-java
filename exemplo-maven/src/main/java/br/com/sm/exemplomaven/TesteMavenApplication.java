b88b83f67a3444d4a6d7fa481331ce808c06d434
Samuel Mozarth <contatosamuelm@gmail.com>
2020-06-04 18:03:42 -0300

    Iniciando estudos em Java

---
+++ exemplo-maven/src/main/java/br/com/sm/exemplomaven/TesteMavenApplication.java
@@ -0,0 +1,13 @@
+package br.com.sm.exemplomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteMavenApplication.class, args);
+	}
+
+}
